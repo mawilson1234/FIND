701e51e55039677a5787aa6555625050882e52a9
Michael Wilson <michael.a.wilson@yale.edu>
2023-08-20 11:26:40 -0400

    fix typo

--- tasks/SCAN/generate_SCAN.py
+++ tasks/SCAN/generate_SCAN.py
@@ -306,7 +306,7 @@ def test_simple_original():
 	Returns a file handle for the existing training split
 	of SCAN's simple task.
 	'''
-	return gzip.open(os.path.join('_original_splits', 'addprim', 'tasks_test_simple.txt.gz'), 'rt')
+	return gzip.open(os.path.join('_original_splits', 'simple', 'tasks_test_simple.txt.gz'), 'rt')
 
 if __name__ == '__main__':
 	# demo
